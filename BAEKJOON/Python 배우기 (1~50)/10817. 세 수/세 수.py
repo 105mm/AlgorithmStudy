19abb91fa67619cd33abe018b959e4126fd10dc6
105mm <king7532@knou.ac.kr>
2023-07-10 00:10:18 +0900

    commit

--- BAEKJOON/Python 배우기 (1~50)/10817. 세 수/세 수.py
+++ BAEKJOON/Python 배우기 (1~50)/10817. 세 수/세 수.py
@@ -1,28 +1,6 @@
-a = list(map(int, input().split()))
-
-max_number = max(a)
-second_largest = None
-
-for number in a:
-    if number != max_number:
-        if second_largest is None or number > second_largest:
-            second_largest = number
-
-if second_largest is None:
-    second_largest = max_number
-
-print(second_largest)
-
-'''
+num_list = list(map(int, input().split()))
+num_list.sort()
+print(num_list[-2])
 
 
-a = list(map(int, input().split()))
-
-if len(a) >= 2:
-    sorted_list = sorted(a)
-    second_largest = sorted_list[-2]
-    print(second_largest) 
-    
-
-    
-'''
+'''완전탐색으로 어떻게 구현해아할까..'''
