b76bd475b495ce093de6465207a8a648f7b4d932
105mm <king7532@knou.ac.kr>
2023-07-14 16:37:48 +0900

    commit

--- BAEKJOON/test.py
+++ BAEKJOON/test.py
@@ -1,1 +1,12 @@
-print(15//2)
+import sys
+
+n = int(sys.stdin.readline())
+
+for _ in range(n):
+    a, b = map(int, sys.stdin.readline().split())
+    m = a*b
+    if a < b:
+        a, b = b, a
+    while a % b != 0:
+        a, b = b, a%b
+    print(int(m/b))
