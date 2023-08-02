6b9c0ce884d6c2ef43c3fb92ce87c9a5445fc921
105mm <king7532@knou.ac.kr>
2023-08-03 00:31:09 +0900

    commit

--- BAEKJOON/Python 배우기 (1~50)/9506. 약수들의 합/약수들의 합.py
+++ BAEKJOON/Python 배우기 (1~50)/9506. 약수들의 합/약수들의 합.py
@@ -1,18 +1,44 @@
-number = float(input())
+while True:
+    number = float(input())
 
-yaksu = []
+    if number == -1:
+        break
 
-if number == -1:
-    break
+    yaksu = []
 
-for i in range (1, (number//2)+1):
-    if number%i == 0:
-        yaksu.append(i)
+    for i in range(1, (int(number) // 2) + 1):
+        if number % i == 0:
+            yaksu.append(i)
 
-yaksusum = " + ".join(map(str, yaksu))
+    yaksusum = " + ".join(map(str, yaksu))
 
-if sum(yaksu) == number:
-    print(str(number) + " = " + str(yaksusum))
+    if sum(yaksu) == number:
+        print(str((int(number))) + " = " + str(yaksusum))
+    else:
+        print(str((int(number))) + " is NOT perfect.")
 
-elif sum(yaksu) != number:
-    print(str(number) + " is NOT perfect.")
+
+
+
+"""
+while True:
+    sum = 0
+    lst = []
+    n = int(input())
+    if n == -1:
+        break
+    for i in range(1, n):
+        if n % i == 0:
+            lst.append(i)
+    for j in lst:
+        sum += j
+    if n == sum:
+        print(f'{n} = {lst[0]}', end= '')
+        for i in range(1, len(lst)):
+            print(f' + {lst[i]}', end='')
+        print()
+    else:
+        print(f'{n} is NOT perfect.')
+    
+
+"""
