21549b87d56645e3c0b6eea23f856c3986861720
105mm <king7532@knou.ac.kr>
2023-08-19 21:12:56 +0900

    commit

---
+++ BAEKJOON/Python 배우기 (1~100)/2438. 별 찍기 - 1/기찍 N.py
@@ -0,0 +1,15 @@
+number = int(input())
+
+for i in range (1,(number+1)):
+    print ("*"*i)
+
+
+
+
+'''
+inp = int(input())
+for i in range(1, (inp+1)):
+    for j in range(1, (i+1)):
+        print("*",end="")
+    print()
+'''
