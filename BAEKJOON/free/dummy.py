01fcc6fb199325025b321f22daa01e38235e88a3
105mm <king7532@knou.ac.kr>
2023-08-29 01:57:16 +0900

    commit

---
+++ BAEKJOON/free/dummy.py
@@ -0,0 +1,43 @@
+import random
+import string
+import os
+
+# 사용할 수 있는 특수문자
+valid_chars = string.ascii_letters + string.digits + r'!@#$%^&()-_+='
+
+# 총 용량 (220GB를 bytes로 변환)
+total_size_gb = 220
+total_size_bytes = total_size_gb * 1024 * 1024 * 1024
+
+# 파일 생성
+files = []
+
+while total_size_bytes > 0:
+    # 랜덤한 용량 생성 (1MB 이상 50MB 이하)
+    file_size = random.randint(1, 50) * 1024 * 1024  # Bytes로 변환
+
+    # 총 용량이 충분한지 확인
+    if total_size_bytes < file_size:
+        break
+
+    # 랜덤한 파일 이름 생성 (10글자로 맞추기)
+    file_name = ''.join(random.choice(valid_chars) for _ in range(10)) + ".txt"
+
+    # 총 용량 조정
+    total_size_bytes -= file_size
+
+    files.append((file_name, file_size))
+"""
+# 출력
+for file_name, file_size in files:
+    output_line = f"D:\\{file_name}\t{str(file_size)}\t{'1'}"
+    print(output_line)
+"""
+# 결과를 D 드라이브 루트 디렉토리에 파일로 저장
+output_path = "D:\\output.txt"
+with open(output_path, "w") as f:
+    for file_name, file_size in files:
+        output_line = f"D:\\{file_name}\t{str(file_size)}\t{'1'}\n"
+        f.write(output_line)
+
+print(f"Results saved to: {output_path}")
