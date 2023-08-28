7b9bdce9d17002574e4a3fc5d204f82350700da6
105mm <king7532@knou.ac.kr>
2023-08-29 02:10:10 +0900

    commit

--- BAEKJOON/free/dummy.py
+++ BAEKJOON/free/dummy.py
@@ -5,8 +5,8 @@ import os
 # 사용할 수 있는 특수문자
 valid_chars = string.ascii_letters + string.digits + r'!@#$%^&()-_+='
 
-# 총 용량 (220GB를 bytes로 변환)
-total_size_gb = 220
+# 총 용량 (bytes로 변환)
+total_size_gb = 56
 total_size_bytes = total_size_gb * 1024 * 1024 * 1024
 
 # 파일 생성
@@ -20,15 +20,15 @@ while total_size_bytes > 0:
     if total_size_bytes < file_size:
         break
 
-    # 랜덤한 파일 이름 생성 (10글자로 맞추기)
-    file_name = ''.join(random.choice(valid_chars) for _ in range(10)) + ".txt"
+    # 랜덤한 파일 이름 생성 (100글자 이상 250글자 이하)
+    file_name = ''.join(random.choice(valid_chars) for _ in range(random.randint(100, 250))) + ".txt"
 
     # 총 용량 조정
     total_size_bytes -= file_size
 
     files.append((file_name, file_size))
 """
-# 출력
+# 출력하고 싶다면
 for file_name, file_size in files:
     output_line = f"D:\\{file_name}\t{str(file_size)}\t{'1'}"
     print(output_line)
