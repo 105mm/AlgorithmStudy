7ce6bbac5b532d95429df9bb5d3783e86f310402
105mm <king7532@knou.ac.kr>
2023-08-29 17:38:22 +0900

    commit

--- BAEKJOON/free/dummy.py
+++ BAEKJOON/free/dummy.py
@@ -6,7 +6,7 @@ import os
 valid_chars = string.ascii_letters + string.digits + r'!@#$%^&()-_+='
 
 # 총 용량 (bytes로 변환)
-total_size_gb = 56
+total_size_gb = 215
 total_size_bytes = total_size_gb * 1024 * 1024 * 1024
 
 # 파일 생성
@@ -20,8 +20,8 @@ while total_size_bytes > 0:
     if total_size_bytes < file_size:
         break
 
-    # 랜덤한 파일 이름 생성 (100글자 이상 250글자 이하)
-    file_name = ''.join(random.choice(valid_chars) for _ in range(random.randint(100, 250))) + ".txt"
+    # 랜덤한 파일 이름 생성 (170글자 이상 190글자 이하)
+    file_name = ''.join(random.choice(valid_chars) for _ in range(random.randint(170, 190))) + ".txt"
 
     # 총 용량 조정
     total_size_bytes -= file_size
@@ -33,11 +33,11 @@ for file_name, file_size in files:
     output_line = f"D:\\{file_name}\t{str(file_size)}\t{'1'}"
     print(output_line)
 """
-# 결과를 D 드라이브 루트 디렉토리에 파일로 저장
-output_path = "D:\\output.txt"
+# 결과를 선택한 드라이브 루트 디렉토리에 파일로 저장
+output_path = "C:\\Users\king7.DESKTOP-F3EIMPM\Desktop\dummy12\output.txt"
 with open(output_path, "w") as f:
     for file_name, file_size in files:
-        output_line = f"D:\\{file_name}\t{str(file_size)}\t{'1'}\n"
+        output_line = f"G:\\{file_name}\t{str(file_size)}\t{'1'}\n"
         f.write(output_line)
 
 print(f"Results saved to: {output_path}")
