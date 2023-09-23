46a8034c4b698606769d4182b9246200bcc0b41a
105mm <king7532@knou.ac.kr>
2023-09-23 17:12:16 +0900

    commit

--- BAEKJOON/2475. 검증수/검증수.py
+++ BAEKJOON/2475. 검증수/검증수.py
@@ -9,3 +9,18 @@ for num in input_numbers:
     list_number.append(int(num) ** 2)
 
 print(sum(list_number) % 10)
+
+
+
+"""
+그냥 ㅓ이렇게 간단하게 해라
+
+num=map(int, input().split())
+sum=0
+for i in num:
+
+    sum+=i**2
+
+print(sum%10)
+
+"""
