a2ccff80c81d448a78d46721cf60f10bb541ac5d
105mm <king7532@knou.ac.kr>
2023-10-21 15:28:52 +0900

    commit

--- BAEKJOON/free/dummy.py
+++ BAEKJOON/free/dummy.py
@@ -6,7 +6,7 @@ import os
 valid_chars = string.ascii_letters + string.digits + r'!@#$%^&()-_+='
 
 # 총 용량 (bytes로 변환) 1111111111111
-total_size_gb = 35
+total_size_gb = 1864
 total_size_bytes = total_size_gb * 1024 * 1024 * 1024
 
 # 파일 생성
@@ -14,7 +14,7 @@ files = []
 
 while total_size_bytes > 0:
     # 랜덤한 용량 생성 (1MB 이상 50MB 이하) 222222222222
-    file_size = random.randint(3, 17) * 1024 * 1024  # Bytes로 변환
+    file_size = random.randint(17, 101) * 1024 * 1024  # Bytes로 변환
 
     # 총 용량이 충분한지 확인
     if total_size_bytes < file_size:
@@ -34,10 +34,20 @@ for file_name, file_size in files:
     print(output_line)
 """
 # 결과를 선택한 드라이브 루트 디렉토리에 파일로 저장
-output_path = "C:\\Users\king7.DESKTOP-F3EIMPM\Desktop\dummy12\output.txt"
+output_path = r"C:\Users\105mm\Desktop\dummy12\output.txt"
 with open(output_path, "w") as f:
     for file_name, file_size in files:
         output_line = f"G:\\{file_name}\t{str(file_size)}\t{'1'}\n" #444444444444444444444 
         f.write(output_line)
 
 print(f"Results saved to: {output_path}")
+
+
+
+"""
+37번 대체 가능
+
+output_path = "C:\\Users\\105mm\\Desktop\\dummy12\\output.txt"
+
+
+"""
