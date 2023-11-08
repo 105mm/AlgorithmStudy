59931d5dbc1774139745e7cc7a65afdc3ab0fa62
105mm <king7532@knou.ac.kr>
2023-11-08 19:42:02 +0900

    commit

---
+++ BAEKJOON/1547. 야바위  사용자정의 배우기/야바위.py
@@ -0,0 +1,37 @@
+import sys
+
+ball = 1
+
+a = int(input())
+
+for _ in range(a):
+    N, K = map(int, sys.stdin.readline().strip().split())
+
+    if N == ball:
+        ball = K
+    elif K == ball:
+        ball = N
+    
+    else:
+        pass
+
+print(ball)
+
+
+
+"""
+def ball(num1, num2):
+    a = result.index(num1)
+    b = result.index(num2)
+    result[a], result[b] = result[b], result[a]
+
+m = int(input())
+result = [1, 2, 3]
+for _ in range(m):
+    a, b = list(map(int, input().split()))
+    ball(a, b)
+
+print(result[0])
+
+
+"""
