871e9bd53e240a4eacfd1069605b8090d7df6b49
105mm <king7532@knou.ac.kr>
2023-11-17 01:41:01 +0900

    commit

---
+++ BAEKJOON/1864. 문어 숫자/문어 숫자.py
@@ -0,0 +1,40 @@
+char_to_num = {'-': 0, '\\': 1, '(': 2, '@': 3, '?': 4, '>': 5, '&': 6, '%': 7, '/': -1}
+
+
+while True:
+
+    num = []
+    x = 0
+    result = 0
+
+    user_input = input()
+    
+    if user_input == '#':
+        break
+    
+    for char in user_input:
+        num.append(char_to_num[char])
+        
+    for i in reversed(num):
+        result += i*8**x
+        x += 1
+    
+    
+    print (result)
+
+
+"""
+
+import sys
+input = sys.stdin.readline
+
+n = "-\\(@?>&%"
+
+while True:
+    s = input().strip()[::-1]
+    if s == '#':
+        break
+
+    print(sum((n.find(s[i])) * 8**i for i in range(len(s))))
+
+"""
