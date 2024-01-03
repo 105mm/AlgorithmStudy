06900f4cf9574a9fcedbb684404e88ab92119833
105mm <king7532@knou.ac.kr>
2024-01-03 11:37:15 +0900

    commit

---
+++ BAEKJOON/4880. 다음차기/다음찾기.py
@@ -0,0 +1,14 @@
+while True:
+
+    N1, N2, N3 = map(int, input().split())
+
+    if N1 == 0 and N2 == 0 and N3 == 0:
+        break
+
+    if N2 - N1 == N3 - N2:
+            
+        print ("AP " + str(N3 + (N2 - N1)))
+
+    elif N2 // N1 == N3 // N2:
+
+        print ("GP " + str(N3 * (N2 // N1)))
