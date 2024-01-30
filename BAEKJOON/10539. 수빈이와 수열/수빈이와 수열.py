0eae7ccda24cad76d43890522c3809891a75bea7
105mm <king7532@knou.ac.kr>
2024-01-30 23:57:49 +0900

    commit

---
+++ BAEKJOON/10539. 수빈이와 수열/수빈이와 수열.py
@@ -0,0 +1,15 @@
+import sys
+
+N1 = int(sys.stdin.readline().strip())
+
+M1 = list(map(int, sys.stdin.readline().strip().split()))
+
+result = []
+
+result.append(M1[0])
+
+for i in range (1, N1):
+
+    result.append((i + 1) * M1[i] - sum(result[:i]))
+
+print(result)
