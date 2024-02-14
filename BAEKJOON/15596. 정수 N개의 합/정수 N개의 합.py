5687fe28583ac9d7e94c7bcef49e041e41863eb4
105mm <king7532@knou.ac.kr>
2024-02-14 23:18:16 +0900

    commit

--- BAEKJOON/15596. 정수 N개의 합/정수 N개의 합.py
+++ BAEKJOON/15596. 정수 N개의 합/정수 N개의 합.py
@@ -1,11 +1,28 @@
+"""
 def solve(a):
     result = 0
     for i in a:
         result += i
     return result
-
+"""
 
 """
 def solve(a):
     return sum(a)
 """
+
+import sys
+
+def solve():
+    # 사용자로부터 입력을 받음
+    input_str = list(map(int, sys.stdin.readline().strip().split()))
+    
+    result = 0
+    for i in input_str:
+        result += i
+    
+    # 결과 출력
+    print("리스트의 합:", result)
+
+# 함수 호출
+solve()
