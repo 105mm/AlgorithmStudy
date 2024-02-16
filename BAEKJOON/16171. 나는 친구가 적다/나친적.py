aadeae8f4ff454301be92f6402e3e17134bc8a01
105mm <king7532@knou.ac.kr>
2024-02-17 00:42:26 +0900

    commit

--- BAEKJOON/16171. 나는 친구가 적다/나친적.py
+++ BAEKJOON/16171. 나는 친구가 적다/나친적.py
@@ -1,15 +1,13 @@
 import sys
 
-check = list(sys.stdin.readline().strip().split())
+check = list(sys.stdin.readline().strip())
 
-while True:
-    count = 0
-    user_input = sys.stdin.readline().strip()
-    if user_input == '#':
-        break
+num = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
 
-    for char in user_input:
-        if char in check:
-            count += 1
-    
-    print(count)
+check_2 = []
+
+for i in range(len(check)):
+    if check[i] not in num:
+        check_2.append(check[i])
+
+print(check_2)
