2c50bbe4ad238b4b8ec508415ec4bf19859ec0bc
105mm <king7532@knou.ac.kr>
2024-03-13 23:14:23 +0900

    commit

--- BAEKJOON/6359. 만취한 상범/만취한 상범.py
+++ BAEKJOON/6359. 만취한 상범/만취한 상범.py
@@ -6,6 +6,7 @@ for _ in range (N):
     a = int(sys.stdin.readline().strip())
     num_list = list(range(1, a+1))
 
+
     for i in range (2, a+1):
         for j in range (1, a//i+1):
             if i*j in num_list:
